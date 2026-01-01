9500dabf857ff7147bae3e1076353950dfefa7e6
namse <namse@users.noreply.github.com>
2026-01-01 14:38:02 +0900

    Remove unused code

--- forte-rs-to-ts/src/main.rs
+++ forte-rs-to-ts/src/main.rs
@@ -20,28 +20,13 @@ use std::process::Command;
 fn main() {
     if env::var("MY_ANALYZER_WRAPPER_MODE").is_ok() {
         let mut args: Vec<String> = env::args().collect();
-
-        let is_build_script = args.iter().any(|arg| arg == "build_script_build");
-
-        if is_build_script {
-            let rustc_path = &args[1];
-            let rustc_args = &args[2..];
-
-            let status = Command::new(rustc_path)
-                .args(rustc_args)
-                .status()
-                .expect("Failed to execute original rustc");
-
-            std::process::exit(status.code().unwrap_or(1));
-        }
-
-        if args.len() > 1 {
-            args.remove(1);
-        }
-        let mut callbacks = Analyzer {
-            ts_output_dir: "../fe/src/pages".to_string(),
-        };
-        run_compiler(&args, &mut callbacks);
+        args.remove(1);
+        run_compiler(
+            &args,
+            &mut Analyzer {
+                ts_output_dir: "../fe/src/pages".to_string(),
+            },
+        );
         return;
     }
     let target_dir = env::args()
